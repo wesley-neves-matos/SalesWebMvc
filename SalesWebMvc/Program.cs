c9ba517b51673acc2cb203dc99c51f4fe6bb4af3
Wesley Neves <wesley.neves.matos@gmail.com>
2024-04-16 08:03:47 -0300

    Seeding Service

--- SalesWebMvc/Program.cs
+++ SalesWebMvc/Program.cs
@@ -1,33 +1,59 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore;
+using Microsoft.EntityFrameworkCore;
 using SalesWebMvc.Data;
-var builder = WebApplication.CreateBuilder(args);
+using SalesWebMvc;
 
-string connectionString = builder.Configuration.GetConnectionString("SalesWebMvcContext");
-builder.Services.AddDbContext<SalesWebMvcContext>(options =>
-    options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)));
 
-// Add services to the container.
-builder.Services.AddControllersWithViews();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+internal class Program
 {
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+    private static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        string connectionString = builder.Configuration.GetConnectionString("SalesWebMvcContext");
+        builder.Services.AddDbContext<SalesWebMvcContext>(options =>
+                     options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)));
+
+        // Add services to the container.
+        builder.Services.AddControllersWithViews();
+
+        builder.Services.AddScoped<SeedingService>();
+
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        if (!app.Environment.IsDevelopment())
+        {
+            app.UseExceptionHandler("/Home/Error");
+            // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+            app.UseHsts();
+        }
+        else
+        {
+            app.UseDeveloperExceptionPage();
+
+            using (var scope = app.Services.CreateScope())
+            {
+                var seedingService = scope.ServiceProvider.GetRequiredService<SeedingService>();
+                seedingService.Seed();
+            }
+        }
+
+        app.UseHttpsRedirection();
+        app.UseStaticFiles();
+
+        app.UseRouting();
+
+        app.UseAuthorization();
+
+        app.MapControllerRoute(
+            name: "default",
+            pattern: "{controller=Home}/{action=Index}/{id?}");
+
+        app.Run();
+    }
+
+
+
 }
-
-app.UseHttpsRedirection();
-app.UseStaticFiles();
-
-app.UseRouting();
-
-app.UseAuthorization();
-
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
-
-app.Run();
