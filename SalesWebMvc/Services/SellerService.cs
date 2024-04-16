71942c5a466c56d70839278f3dbeb79e5e753b6f
Wesley Neves <wesley.neves.matos@gmail.com>
2024-04-16 13:43:06 -0300

    Delete Seller

--- SalesWebMvc/Services/SellerService.cs
+++ SalesWebMvc/Services/SellerService.cs
@@ -14,9 +14,18 @@ namespace SalesWebMvc.Services
 
         public List<Seller> FindAll() => _context.Seller.OrderBy(i => i.Name).ToList();
 
+        public Seller FindById(int id) => _context.Seller.FirstOrDefault(i => i.Id == id);
+
         public void Insert(Seller seller)
         {
             _context.Seller.Add(seller);
+            _context.SaveChanges();
+        }
+
+        public void Remove(int id)
+        {
+            var seller=_context.Seller.Find(id);
+            _context.Seller.Remove(seller);
             _context.SaveChanges();
         }
     }
