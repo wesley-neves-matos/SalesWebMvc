4d0bb6aad21b465277f106230a51d4c9b085542e
Wesley Neves <wesley.neves.matos@gmail.com>
2024-04-16 16:44:45 -0300

    Validation

--- SalesWebMvc/Models/Seller.cs
+++ SalesWebMvc/Models/Seller.cs
@@ -6,21 +6,29 @@ namespace SalesWebMvc.Models
     {
         public int Id { get; set; }
 
+        [Required(ErrorMessage = "{0} required!")]
+        [StringLength(60, MinimumLength = 3, ErrorMessage = "{0} size should be between {2} and {1}")]
         public string Name { get; set; }
 
+        [Required(ErrorMessage = "{0} required!")]
+        [EmailAddress(ErrorMessage = "Enter a valid email!")]
         [DataType(DataType.EmailAddress)]
         public string Email { get; set; }
 
-        [Display(Name="Birth Date")]
+        [Required(ErrorMessage = "{0} required!")]
+        [Display(Name = "Birth Date")]
         [DataType(DataType.Date)]
-
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
         public DateTime BirthDate { get; set; }
 
+        [Required(ErrorMessage = "{0} required!")]
+        [Range(100.0, 50000.0, ErrorMessage ="{0} must be from {1} to {2}")]
         [DataType(DataType.Currency)]
+        [DisplayFormat(DataFormatString = "{0:C2}")]
         public decimal Salary { get; set; }
 
-        public int DepartmentId { get; set; }
         public Department Department { get; set; }
+        public int DepartmentId { get; set; }
         ICollection<SalesRecord> SalesRecords { get; set; } = new List<SalesRecord>();
 
         public Seller()
